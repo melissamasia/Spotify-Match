83cc4fc40676e9461819d927d57ba32bd6a47107
Melissa Masia <melissamasia@gmail.com>
2018-12-06 10:04:10 -0600

    added show_dialog param to allow 2 users in the same browser

--- src/PopupWindow.js
+++ src/PopupWindow.js
@@ -5,7 +5,7 @@ class PopupWindow extends Component {
     render(){
         return(
             <NewWindow
-              url={'https://accounts.spotify.com/en/authorize?client_id=b69a89e58df54733aa251e2bb81537fb&redirect_uri=http:%2F%2Flocalhost:3000%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=123'}
+              url={'https://accounts.spotify.com/en/authorize?client_id=b69a89e58df54733aa251e2bb81537fb&redirect_uri=http:%2F%2Flocalhost:3000%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=123&show_dialog=true'}
             >
             </NewWindow>
         );
